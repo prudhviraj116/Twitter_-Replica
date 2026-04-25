63595e24b8bbc2964e583abddde198d822815b08
prudhviraj116 <prudhvirajsuthapalli@gmail.com>
2026-04-25 17:22:18 +0530

    Delete prudhvi directory

--- prudhvi/javascript/additional_functionality.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Optionally, you can add any functionality here
-    // For example, redirecting the user after a delay
-    setTimeout(function() {
-        window.location.href = "royalenfield.html"; // Redirect to the main page
-    }, 5000); // Redirect after 5 seconds (adjust as needed)
-});
