1ee36e8f50d1390caf5f44abc4225641ecc8ddcd
prudhviraj116 <prudhvirajsuthapalli@gmail.com>
2026-04-25 17:22:32 +0530

    Delete travelling project directory

--- travelling project/project007/WebApp/DBApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns =[
-    path('insert/',views.dbactions,name='empinserturl'),
-    path('select/', views.selectdata,name='empselecturl'),
-    path('update/<int:eno>', views.updatedata,name='empupdateurl'),
-    path('delete/<int:eno>', views.deletedata,name='empdeleteurl'),
-
- ]
--- travelling project/project007/WebApp/MathTable/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MathtableConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MathTable'
